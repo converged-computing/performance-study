9f738e34a7b6b3136458aadf0eacaf5883048258
Daniel Milroy <daniel.milroy@gmail.com>
2024-08-23 01:42:45 -0700

    pcluster: add kripke, laghos, lammps, minife

---
+++ experiments/aws/parallel-cluster/cpu/experiment/configs/laghos/slurm-laghos-128n.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#SBATCH --job-name=laghos-128n
+#SBATCH --nodes=128
+#SBATCH --time=0:10:00
+#SBATCH --exclusive
+
+echo "Start time:" $( date +%s )
+/usr/bin/time -p mpirun -N 128 --map-by ppr:96:node \
+        singularity exec /shared/containers/metric-laghos_libfabric-cpu-zen4.sif /opt/laghos/laghos \
+        -pa -p 1 -tf 0.6 -pt 311 -m /opt/laghos/data/cube_311_hex.mesh --ode-solver 7 --max-steps 400 --cg-tol 0 -cgm 50 -ok 3 -ot 2 -rs 4 -rp 2 --fom
+echo "End time:" $( date +%s )
