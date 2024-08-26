a4c1c0f3df839febdf70e697d3baaf71c3e7bd78
Abhik Sarkar <sarkar6@llnl.gov>
2024-08-26 05:31:36 +0000

    Quicksilver cyclecloud updates

---
+++ experiments/azure/cyclecloud/cpu/experiment/configs/quicksilver/slurm-quicksilver-32n.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#SBATCH --job-name=quicksilver-32n
+#SBATCH --nodes=32
+#SBATCH --output=qs-output-N32n1024t3-slurm.txt
+#SBATCH --error=qs-error--N32n1024t3-slurm.txt
+#SBATCH --time=0:10:00
+#SBATCH --exclusive
+
+. /etc/profile.d/modules.sh
+module unload mpi
+ml mpi/hpcx-pmix-2.18
+
+echo "Start time:" $( date +%s )
+export OMP_NUM_THREADS=3
+/usr/bin/time -p mpirun -N 32 -x UCX_POSIX_USE_PROC_LINK=n --map-by ppr:32:node \
+    singularity exec --env OMP_NUM_THREADS=3 --env UCX_TLS=ud_x,rc_mlx5,cma --env UCX_UNIFIED_MODE=y --env UCX_NET_DEVICES=mlx5_ib0:1 --env OPAL_PREFIX= \
+    /shared/containers/metric-quicksilver-cpu_azure-hpc.sif qs \
+    --inputFile /opt/quicksilver/Examples/CORAL2_Benchmark/Problem1/Coral2_P1.inp \
+    -X 128 -Y 128 -Z 64 -x 128 -y 128 -z 64 -I 16 -J 8 -K 8 -n 335544320
+echo "End time:" $( date +%s )
