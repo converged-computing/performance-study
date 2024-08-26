c0e9a17c390a259a871bc526da089512b8131e5c
Abhik Sarkar <sarkar6@llnl.gov>
2024-08-26 06:06:17 +0000

    Adding cpus-per-proc to quicksilver scripts

--- experiments/azure/cyclecloud/cpu/experiment/configs/quicksilver/slurm-quicksilver-2n.sh
+++ experiments/azure/cyclecloud/cpu/experiment/configs/quicksilver/slurm-quicksilver-2n.sh
@@ -12,7 +12,7 @@ ml mpi/hpcx-pmix-2.18
 
 echo "Start time:" $( date +%s )
 export OMP_NUM_THREADS=3
-/usr/bin/time -p mpirun -N 2 -x UCX_POSIX_USE_PROC_LINK=n --map-by ppr:32:node \
+/usr/bin/time -p mpirun -N 2 --cpus-per-proc 3 -x UCX_POSIX_USE_PROC_LINK=n --map-by ppr:32:node \
     singularity exec --env OMP_NUM_THREADS=3 --env UCX_TLS=ud_x,rc_mlx5,cma --env UCX_UNIFIED_MODE=y --env UCX_NET_DEVICES=mlx5_ib0:1 --env OPAL_PREFIX= \
     /shared/containers/metric-quicksilver-cpu_azure-hpc.sif qs \
     --inputFile /opt/quicksilver/Examples/CORAL2_Benchmark/Problem1/Coral2_P1.inp \
