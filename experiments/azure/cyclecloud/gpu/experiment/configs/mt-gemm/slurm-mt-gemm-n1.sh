f1a3ba8e1687ade16c722228d551765225683173
Abhik Sarkar <sarkar6@llnl.gov>
2024-08-29 22:34:50 +0000

    Changes to scripts tried on 2 nodes on 08/28

---
+++ experiments/azure/cyclecloud/gpu/experiment/configs/mt-gemm/slurm-mt-gemm-n1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#SBATCH --job-name=mixbench-2n
+#SBATCH --nodes=2
+#SBATCH --time=0:20:00
+#SBATCH --exclusive
+
+. /etc/profile.d/modules.sh
+ml mpi/hpcx-pmix-2.18
+#ml cuda/11.8.0 #xl/2023.06.28-cuda-11.8.0-gcc-11.2.1
+
+echo "Start time:" $( date +%s )
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+/usr/bin/time -p mpirun -n 1 -x UCX_POSIX_USE_PROC_LINK=n --map-by ppr:8:node \
+    singularity exec --env UCX_TLS=ud_x,rc_mlx5,cma --env UCX_UNIFIED_MODE=y --env UCX_NET_DEVICES=mlx5_ib0:1 --env OPAL_PREFIX= \
+     /shared/containers/mt-gemm_azure-hpc-gpu-ubuntu2204.sif /opt/gem/mt-dgemm.x 100 2048
+echo "End time:" $( date +%s )
---
+++ experiments/azure/cyclecloud/gpu/experiment/configs/osu/slurm-osu-8n-gpu.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#SBATCH --job-name=osu-8n
+#SBATCH --nodes=8
+#SBATCH --time=0:30:00
+#SBATCH --exclusive
+
+. /etc/profile.d/modules.sh
+module unload mpi
+ml mpi/hpcx-pmix-2.18
+#ml cuda/11.8.0 #xl/2023.06.28-cuda-11.8.0-gcc-11.2.1
+
+echo "Start time:" $( date +%s )
+mpirun --map-by ppr:1:node \
+     /shared/bin/singularity exec --env UCX_UNIFIED_MODE=y --env UCX_NET_DEVICES=mlx5_ib0:1 --env OPAL_PREFIX= \
+     /shared/containers/metric-osu-gpu_azure-hpc-gpu-ubuntu2204.sif /opt/osu-benchmark/build.openmpi/mpi/collective/osu_allreduce -d cuda D D    
+     #/shared/containers/metric-osu-gpu_azure-hpc-gpu-ubuntu2204.sif /opt/osu-benchmark/build.openmpi/mpi/pt2pt/osu_latency -d cuda D D    
+echo "End time:" $( date +%s )
