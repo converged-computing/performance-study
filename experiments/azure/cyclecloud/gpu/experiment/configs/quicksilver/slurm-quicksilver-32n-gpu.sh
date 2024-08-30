cff01f756f3696e076929698ece71645f7bedf94
Abhik Sarkar <sarkar6@llnl.gov>
2024-08-30 22:16:04 +0000

    Updated quicksilver scripts

--- experiments/azure/cyclecloud/gpu/experiment/configs/quicksilver/slurm-quicksilver-32n-gpu.sh
+++ experiments/azure/cyclecloud/gpu/experiment/configs/quicksilver/slurm-quicksilver-32n-gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#SBATCH --job-name=quicksilver-4n
-#SBATCH --nodes=4
+#SBATCH --job-name=quicksilver-32n
+#SBATCH --nodes=32
 #SBATCH --time=0:20:00
 #SBATCH --exclusive
 
@@ -11,9 +11,9 @@ ml mpi/hpcx-pmix-2.18
 
 echo "Start time:" $( date +%s )
 export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-/usr/bin/time -p mpirun -n 32 -x UCX_POSIX_USE_PROC_LINK=n --map-by ppr:8:node \
+/usr/bin/time -p mpirun -n 256 -x UCX_POSIX_USE_PROC_LINK=n --map-by ppr:8:node \
     singularity exec --env UCX_TLS=ud_x,rc_mlx5,cma --env UCX_UNIFIED_MODE=y --env UCX_NET_DEVICES=mlx5_ib0:1 --env OPAL_PREFIX= \
     /shared/containers/metrioc-quicksilver-gpu_azure-hpc-gpu-ubuntu2204.sif qs \
     --inputFile /opt/quicksilver/Examples/CORAL2_Benchmark/Problem1/Coral2_P1.inp \
-     -X 32 -Y 32 -Z 32 -x 32 -y 32 -z 32 -I 4 -J 4 -K 2 -n 52428800
+    -X 64  -Y 64  -Z 64  -x 64  -y 64  -z 64  -I 8  -J 8  -K 4 -n 419430400
 echo "End time:" $( date +%s )
