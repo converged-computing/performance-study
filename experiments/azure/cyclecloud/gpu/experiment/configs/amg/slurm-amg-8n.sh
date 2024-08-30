8cc227db2923716586a079c870c3306b6c4ed3d7
Abhik Sarkar <sarkar6@llnl.gov>
2024-08-30 22:33:39 +0000

    Updated AMG scripts

--- experiments/azure/cyclecloud/gpu/experiment/configs/amg/slurm-amg-8n.sh
+++ experiments/azure/cyclecloud/gpu/experiment/configs/amg/slurm-amg-8n.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#SBATCH --job-name=amg-2n
-#SBATCH --nodes=2
+#SBATCH --job-name=amg-8n
+#SBATCH --nodes=8
 #SBATCH --time=0:20:00
 #SBATCH --exclusive
 
@@ -11,10 +11,10 @@ module load mpi/hpcx-pmix-2.18
 
 echo "Start time:" $( date +%s )
 export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-/usr/bin/time -p mpirun -n 16 --map-by ppr:8:node -x UCX_POSIX_USE_PROC_LINK=n \
+/usr/bin/time -p mpirun -n 64 --map-by ppr:8:node -x UCX_POSIX_USE_PROC_LINK=n \
                  singularity exec --env UCX_TLS=ud,shm,rc \
 		 --env UCX_UNIFIED_MODE=y --env UCX_NET_DEVICES=mlx5_ib0:1 --env OPAL_PREFIX= \
 		/shared/containers/metric-amg2023_azure-hpc-gpu-ubuntu2204.sif \
                 /opt/spack-environment/.spack-env/view/bin/amg \
-		-n 256 256 128 -P 4 2 2 -problem 2
+		-n 256 256 128 -P 4 4 4 -problem 2
 echo "End time:" $( date +%s )
