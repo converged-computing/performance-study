bd586e78c8ca6169ba58f80341b1102ad084c651
Abhik Sarkar <sarkar6@llnl.gov>
2024-08-30 21:19:39 +0000

    Updated mixbench scripts

--- experiments/azure/cyclecloud/gpu/experiment/configs/mixbench/slurm-mixbench-32n.sh
+++ experiments/azure/cyclecloud/gpu/experiment/configs/mixbench/slurm-mixbench-32n.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#SBATCH --job-name=mixbench-2n
-#SBATCH --nodes=2
+#SBATCH --job-name=mixbench-32n
+#SBATCH --nodes=32
 #SBATCH --time=0:20:00
 #SBATCH --exclusive
 
@@ -11,7 +11,7 @@ ml mpi/hpcx-pmix-2.18
 
 echo "Start time:" $( date +%s )
 export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-/usr/bin/time -p mpirun -n 2 -x UCX_POSIX_USE_PROC_LINK=n --map-by ppr:1:node \
+/usr/bin/time -p mpirun -n 32 -x UCX_POSIX_USE_PROC_LINK=n --map-by ppr:1:node \
     singularity exec --env UCX_TLS=ud_x,rc_mlx5,cma --env UCX_UNIFIED_MODE=y --env UCX_NET_DEVICES=mlx5_ib0:1 --env OPAL_PREFIX= \
     /shared/containers/metric-mixbench_azure-hpc-gpu-ubuntu2204.sif /opt/mixbench/mixbench-cuda/wrapper 32 \
 echo "End time:" $( date +%s )
